838331212dc21b0aa66c4fc16c391dc929fae060
LynAs <szlynas@gmail.com>
2018-12-01 15:38:54 +0600

    create/list book done

--- src/main/kotlin/com/lynas/crud/CrudApplication.kt
+++ src/main/kotlin/com/lynas/crud/CrudApplication.kt
@@ -2,6 +2,7 @@ package com.lynas.crud
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ModelAttribute
@@ -27,18 +28,23 @@ data class Book(
         var name: String
 )
 
+interface BookRepository : JpaRepository<Book,Long>
+
 @Controller
-class HomeController {
+class HomeController(val bookRepository: BookRepository) {
 
     @RequestMapping("/")
     fun home(model: Model): String {
         model.addAttribute("book", Book(name = ""))
+        val bookList = bookRepository.findAll()
+        model.addAttribute("bookList", bookList)
+
         return "home"
     }
 
     @PostMapping("/book")
     fun createBook(@ModelAttribute book: Book): String {
-        println(book.name)
+        bookRepository.save(book)
         return "redirect:/"
     }
 }
